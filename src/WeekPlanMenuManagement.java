ae72018691f55903f4d6f243104d70ea09da1725
leesangkwon <sangkwon960110@gmail.com>
2021-03-10 13:45:09 +0900

    removed WeekPlamManager.java and added WeekPlamMenuManagement.java

---
+++ src/WeekPlanMenuManagement.java
@@ -0,0 +1,10 @@
+
+public class WeekPlanMenuManagement {
+	public static void main(String[]args) {
+		System.out.println("1.Add WeekPlan ");
+		System.out.println("2.Delete WeekPlan ");
+		System.out.println("3.View WeekPlan ");
+		System.out.println("4.Exit ");
+	}
+
+}
