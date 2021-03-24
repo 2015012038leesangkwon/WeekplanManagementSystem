7009e11826e3715587cc0e3a36e215dcfebe5a91
leesangkwon <sangkwon960110@gmail.com>
2021-03-24 16:57:36 +0900

    add munu selection

--- src/WeekPlanMenuManagement.java
+++ src/WeekPlanMenuManagement.java
@@ -1,39 +1,36 @@
 import java.util.Scanner;
 
 public class WeekPlanMenuManagement {
-	public static void main(String[]args) {
+	public static void main(String[]args) {		
 		int num =0;
 		Scanner input = new Scanner(System.in);
 		
-		while(num !=4) {	
-		System.out.println("1.Add WeekPlan ");
-		System.out.println("2.Delete WeekPlan ");
-		System.out.println("3.View WeekPlan ");
-		System.out.println("4.Exit ");
-		System.out.print("select one number between 1-4: ");
+		while(num !=5) {	
+		System.out.println("1.Add Weekly Plan ");
+		System.out.println("2.Delete Weekly Plan ");
+		System.out.println("3.View Weekly Plan ");
+		System.out.println("4.Edit Weekly Plan ");
+		System.out.println("5.EXit ");
+		System.out.print("select one number between 1-5:");
 		num = input.nextInt();
 		switch(num) {
 		case 1:
-			System.out.print("Week date : ");
-		    String weekdate = input.next();
-			System.out.print("Week plan : ");
-			String weekplan = input.next();
-			System.out.print("when week plan end ?  : ");
-		    String enddate = input.next();
+			addWeekplan();
+			
 			break;
 		case 2:
-			System.out.print("which Week plan delete? : ");
-			String deleteweekplan = input.next();
+			deleteWeekplan();
 			break;
 			
 		case 3:
-			System.out.print("which Week plan want to view? : ");
-			String viewweekplan = input.next();
+			viewWeekplan();
 			break;
 		case 4:
-			System.out.print("Exit");
+			EditWeekplan();
 			break;
-			
+		case 5:
+			System.out.println("Exit");
+			break;
 		default :
 			System.out.println("ERROR!!!!");
 			break;
@@ -41,8 +38,47 @@ public class WeekPlanMenuManagement {
 			
 		
 		}
+		
 		}
+		
+
 	}
+	public static void addWeekplan() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Week date : ");
+	    String weekdate = input.next();
+		System.out.print("Week plan : ");
+		String weekplan = input.next();
+		System.out.print("When week plan end? : ");
+	    String enddate = input.next();
+		
+	}
+	public static void deleteWeekplan() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Which day plan delete? : ");
+		String deletedayweekplan = input.next();
+		System.out.print("What delete Week Plan ? : ");
+		String deleteweekplan = input.next();
+		
+	}
+	public static void viewWeekplan() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Which day plan View? : ");
+		String viewdayweekplan = input.next();
+		System.out.print("which Week plan want to view? : ");
+		String viewweekplan = input.next();
+	}
+	public static void EditWeekplan() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Which day plan Edit? : ");
+		String editdayweekplan = input.next();
+		System.out.print("What edit Week Plan ? : ");
+		String editweekplan = input.next();
+		
+	}
+
+
+
 
 
 }
