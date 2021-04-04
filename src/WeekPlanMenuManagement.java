c7ae6ad4e6ca617b34a127ad3150755da00fdcd9
leesangkwon <sangkwon960110@gmail.com>
2021-04-04 15:41:20 +0900

    add menu selection

--- src/WeekPlanMenuManagement.java
+++ src/WeekPlanMenuManagement.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class WeekPlanMenuManagement {
 	public static void main(String[]args) {		
-		int num =0;
+			
 		Scanner input = new Scanner(System.in);
+		WeekPlanManager weekplanmanager = new WeekPlanManager(input);
+		int num =-1;
 		
 		while(num !=5) {	
 		System.out.println("1.Add Weekly Plan ");
@@ -15,18 +17,18 @@ public class WeekPlanMenuManagement {
 		num = input.nextInt();
 		switch(num) {
 		case 1:
-			addWeekplan();
+			weekplanmanager.addWeekplan();
 			
 			break;
 		case 2:
-			deleteWeekplan();
+			weekplanmanager.deleteWeekplan();
 			break;
 			
 		case 3:
-			viewWeekplan();
+			weekplanmanager.viewWeekplan();
 			break;
 		case 4:
-			EditWeekplan();
+			weekplanmanager.EditWeekplan();
 			break;
 		case 5:
 			System.out.println("Exit");
@@ -43,40 +45,7 @@ public class WeekPlanMenuManagement {
 		
 
 	}
-	public static void addWeekplan() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Week date : ");
-	    String weekdate = input.next();
-		System.out.print("Week plan : ");
-		String weekplan = input.next();
-		System.out.print("When week plan end? : ");
-	    String enddate = input.next();
-		
-	}
-	public static void deleteWeekplan() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Which day plan delete? : ");
-		String deletedayweekplan = input.next();
-		System.out.print("What delete Week Plan ? : ");
-		String deleteweekplan = input.next();
-		
-	}
-	public static void viewWeekplan() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Which day plan View? : ");
-		String viewdayweekplan = input.next();
-		System.out.print("which Week plan want to view? : ");
-		String viewweekplan = input.next();
-	}
-	public static void EditWeekplan() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Which day plan Edit? : ");
-		String editdayweekplan = input.next();
-		System.out.print("What edit Week Plan ? : ");
-		String editweekplan = input.next();
-		
-	}
-
+	
 
 
 
