0559338aaeecb32015f726a7bd4805e515b204e3
leesangkwon <sangkwon960110@gmail.com>
2021-04-06 15:02:12 +0900

    added MaterialmechanicsHomeWork.java

---
+++ src/WeekPlan/MaterialmechanicsHomwork.java
@@ -0,0 +1,41 @@
+package WeekPlan;
+
+import java.util.Scanner;
+
+public class MaterialmechanicsHomwork extends Weekplan {
+	public void getUserInput(Scanner input) {
+		System.out.print("number : ");
+	    int weeknum = input.nextInt();
+	    this.setWeeknum(weeknum);
+		
+	    char answer= 'x';
+	    while(answer !='y'&&answer!='Y'&&answer!='n'&&answer!='N') {
+	    System.out.print("기간이 정해진 과제입니까? (Y/N) : ");
+	    answer = input.next().charAt(0);
+	    if(answer == 'Y'||answer == 'y') {
+	    	System.out.print("Week date : ");
+	    	int sdate = input.nextInt();
+	 	    this.setSdate(sdate);
+	 	   System.out.print("Week plan : ");
+			String weekplan = input.next();
+			this.setWeekplan(weekplan);
+	 	   System.out.print("When week plan end? : ");
+		    int edate = input.nextInt();
+		    this.setEdate(edate);
+		    break;
+	    }
+	    else if(answer == 'n' || answer == 'N') {
+	    	this.setSdate(0);
+	    	this.setWeekplan("");
+	    	this.setEdate(0);
+	    	break;
+	    }
+	    else {
+	    	
+	    }
+	    }
+		
+		
+	}
+
+}
