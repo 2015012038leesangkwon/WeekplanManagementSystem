2de3032a13f5083a167335616a97eaea3526ccfd
leesangkwon <sangkwon960110@gmail.com>
2021-04-28 15:57:14 +0900

    added comprehensive design

--- src/WeekPlan/ComprehensivedesignReport.java
+++ src/WeekPlan/ComprehensivedesignReport.java
@@ -3,6 +3,9 @@ package WeekPlan;
 import java.util.Scanner;
 
 public class ComprehensivedesignReport extends Weekplan {
+	public ComprehensivedesignReport(Weekplanfind kind) {
+		super(kind);
+	}
 	public void getUserInput(Scanner input) {
 		System.out.print("number : ");
 	    int weeknum = input.nextInt();
