27a14b3d7135738b756910e1705eb87bebd8d027
leesangkwon <sangkwon960110@gmail.com>
2021-05-03 17:26:40 +0900

    added Week Plan

--- src/WeekPlan/Weekplan.java
+++ src/WeekPlan/Weekplan.java
@@ -2,7 +2,7 @@ package WeekPlan;
 
 import java.util.Scanner;
 
-public class Weekplan {
+public abstract class Weekplan {
 	
 
 
@@ -73,40 +73,8 @@ public class Weekplan {
 		this.edate = edate;
 	}
 
+	public abstract void printInfo();
+
 	
-	public void printInfo() {
-		String skind = "none";
-		switch(this.kind) {
-		case OOPHomework:
-			skind = "OOP";
-			break;
-		case MaterialmechanicsHomwork:
-			skind = "Materialmechanics";
-			break;
-		case ComprehensivedesignReport:
-			skind = "ComprehensivedesignReport";
-			break;
-		case etc:
-			skind = "etc";
-			break;
-		default:
-			
-		}
-		System.out.println ("number : " + weeknum +" /Object : "+ skind+ " /Week plan : "+ weekplan+ " /Week date : " + sdate + " /End date : " + edate);
-	}
-	public void getUserInput(Scanner input) {
-		System.out.print("number : ");
-	    int weeknum = input.nextInt();
-	    this.setWeeknum(weeknum);
-		System.out.print("Week date : ");
-	    int sdate = input.nextInt();
-	    this.setSdate(sdate);
-		System.out.print("Week plan : ");
-		String weekplan = input.next();
-		this.setWeekplan(weekplan);
-		System.out.print("When week plan end? : ");
-	    int edate = input.nextInt();
-	    this.setEdate(edate);
-	}
 
 }
