fe5f83303d8a4a195431bac39411b3b32d985b01
leesangkwon <sangkwon960110@gmail.com>
2021-05-03 17:27:29 +0900

    added Week Plan Manager

--- src/WeekPlanManager.java
+++ src/WeekPlanManager.java
@@ -7,9 +7,10 @@ import WeekPlan.MaterialmechanicsHomwork;
 import WeekPlan.OOP;
 import WeekPlan.Weekplan;
 import WeekPlan.Weekplanfind;
+import WeekPlan.Weekplaninput;
 
 public class WeekPlanManager {
-	ArrayList<Weekplan> weekplans = new ArrayList<Weekplan>();
+	ArrayList<Weekplaninput> weekplans = new ArrayList<Weekplaninput>();
 	Scanner input;
 	WeekPlanManager(Scanner input){
 		this.input = input;
@@ -17,7 +18,7 @@ public class WeekPlanManager {
 	}
 	public void addWeekplan() {	
 		int kind =0;
-		Weekplan weekplan1;
+		Weekplaninput weekplaninput;
 		while(kind!=1 && kind !=2 && kind !=3 && kind!=4) {
 			System.out.println("1 for OOP Homework");
 			System.out.println("2 for Material Mechanics Homwork");
@@ -26,28 +27,28 @@ public class WeekPlanManager {
 			System.out.print("Select Week Plan Kind between1 and 4 : ");
 			kind = input.nextInt();
 			if(kind ==1) {
-				weekplan1 = new OOP(Weekplanfind.OOPHomework);
-				weekplan1.getUserInput(input);
-				weekplans.add(weekplan1);
+				weekplaninput = new OOP(Weekplanfind.OOPHomework);
+				weekplaninput.getUserInput(input);
+				weekplans.add(weekplaninput);
 				break;
 				
 			}
 			else if(kind ==2) {
-				weekplan1 = new MaterialmechanicsHomwork(Weekplanfind.MaterialmechanicsHomwork);
-				weekplan1.getUserInput(input);
-				weekplans.add(weekplan1);
+				weekplaninput = new MaterialmechanicsHomwork(Weekplanfind.MaterialmechanicsHomwork);
+				weekplaninput.getUserInput(input);
+				weekplans.add(weekplaninput);
 				break;
 			}
 			else if(kind ==3) {
-				weekplan1 =new ComprehensivedesignReport(Weekplanfind.ComprehensivedesignReport);
-				weekplan1.getUserInput(input);
-				weekplans.add(weekplan1);
+				weekplaninput =new ComprehensivedesignReport(Weekplanfind.ComprehensivedesignReport);
+				weekplaninput.getUserInput(input);
+				weekplans.add(weekplaninput);
 				break;
 			}
 			else if(kind ==4) {
-				weekplan1 =  new Etc(Weekplanfind.etc);
-				weekplan1.getUserInput(input);
-				weekplans.add(weekplan1);
+				weekplaninput =  new Etc(Weekplanfind.etc);
+				weekplaninput.getUserInput(input);
+				weekplans.add(weekplaninput);
 				break;
 			}
 			else {
@@ -98,7 +99,7 @@ public class WeekPlanManager {
 		int editweekplannum = input.nextInt();
 		for(int i =0; i<weekplans.size(); i++) {
 			
-			Weekplan weekplan = weekplans.get(i);
+			Weekplaninput weekplaninput = weekplans.get(i);
 			if(weekplans.get(i).getWeeknum() == editweekplannum) {	
 				int num =-1;
 				while(num !=4) {	
@@ -113,18 +114,18 @@ public class WeekPlanManager {
 					if(num == 1){
 						System.out.print("Start date :");
 						int sdates = input.nextInt();
-						weekplan.setSdate(sdates);
+						weekplaninput.setSdate(sdates);
 					}
 						
 					else if(num == 2) {
 						System.out.print("Week plan : ");
 						String weekplan1 = input.next();
-						weekplan.setWeekplan(weekplan1);
+						weekplaninput.setWeekplan(weekplan1);
 					}					
 					else if(num ==3) {						
 						System.out.print("End plan :");
 						int edates = input.nextInt();
-						weekplan.setEdate(edates);
+						weekplaninput.setEdate(edates);
 					}
 					else if(num ==4) {
 						System.out.println("Exit");
