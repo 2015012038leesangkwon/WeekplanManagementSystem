70f5e781b6cf4ff005da67f50c77a4cdafd97048
leesangkwon <sangkwon960110@gmail.com>
2021-06-02 17:22:47 +0900

    added Weekly Plan Viewer

--- src/GUI/WeekPlanVeiwer.java
+++ src/GUI/WeekPlanVeiwer.java
@@ -1,28 +1,48 @@
 package GUI;
 
+import java.util.Vector;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class WeekPlanVeiwer extends JFrame {
+import Manager.WeekPlanManager;
+import WeekPlan.Weekplan;
+import WeekPlan.Weekplaninput;
 
-	public  WeekPlanVeiwer() {
+public class WeekPlanVeiwer extends JPanel {
+	Windowframe f;
+	 WeekPlanManager w;
+
+	public  WeekPlanVeiwer(Windowframe f,  WeekPlanManager w) {
+		this.f = f;
+		this.w = w;
+		
+		System.out.println("***"+w.size()+"***");
+		
+		
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("num");
 		model.addColumn("Started Date");
 		model.addColumn("Weekly Plan");
 		model.addColumn("Ended Date");
 		
+		for(int i=0; i<w.size(); i++) {
+			Vector row = new Vector();
+			Weekplaninput wpi = w.get(i);
+			row.add(wpi.getWeeknum());
+			row.add(wpi.getSdate());
+			row.add(wpi.getWeekplan());
+			row.add(wpi.getEdate());
+			model.addRow(row);
+		}
+		
 		JTable t = new JTable(model);
 		JScrollPane sp = new JScrollPane(t);
 		
 		this.add(sp);
-		 this.setSize(300,300);
-		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
-		
-		 this.setVisible(true);
+			
 	}
-
 }
