f40758d021b6c580cf10f2bbfeb4b9ca49fe86b5
leesangkwon <sangkwon960110@gmail.com>
2021-06-02 17:21:56 +0900

    added Menu Selection

--- src/GUI/MenuSelection.java
+++ src/GUI/MenuSelection.java
@@ -7,11 +7,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MenuSelection extends JFrame {
+import Listener.ButtonAddListener;
+import Listener.ButtonViewListener;
+
+public class MenuSelection extends JPanel {
+	Windowframe f;
 	
-	public MenuSelection() {
-		this.setSize(300,300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public MenuSelection(Windowframe f) {
+		this.f = f;
+		
+		this.setLayout(new BorderLayout());
+		
 		JPanel p = new JPanel();
 		JPanel p2 = new JPanel();
 		JLabel l = new JLabel("Menu Selection");
@@ -22,14 +28,16 @@ public class MenuSelection extends JFrame {
 		JButton botton4 = new JButton("Edit Weekly Plan");
 		JButton botton5 = new JButton("EXIT");
 		
+		botton1.addActionListener(new ButtonAddListener(f));
+		botton3.addActionListener(new ButtonViewListener(f));
+		
 		p2.add(l);
 		p.add(botton1);
 		p.add(botton2);
 		p.add(botton3);
 		p.add(botton4);
 		p.add(botton5);
-		
-		
+				
 		this.add(p2, BorderLayout.NORTH);
 		this.add(p, BorderLayout.CENTER);
 		this.setVisible(true);
