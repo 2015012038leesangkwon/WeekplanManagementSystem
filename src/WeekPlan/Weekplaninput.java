2a5ad6a149c20a9c10f900c82c9c25fc9ce3d634
leesangkwon <sangkwon960110@gmail.com>
2021-06-02 17:27:53 +0900

    added WeekPlanInput

--- src/WeekPlan/Weekplaninput.java
+++ src/WeekPlan/Weekplaninput.java
@@ -9,6 +9,9 @@ public interface Weekplaninput {
 public void getUserInput(Scanner input);
 	
 	public int getWeeknum();
+	public int getSdate();
+	public String getWeekplan();
+	public int getEdate();
 	
 	public void setSdate(int sdate);
 	
