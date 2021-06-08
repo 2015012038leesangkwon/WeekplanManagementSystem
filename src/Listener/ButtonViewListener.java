96a9eea98d9dc0bf964ae42feeaff54fbf7f2541
leesangkwon <sangkwon960110@gmail.com>
2021-06-08 17:57:20 +0900

    added button  view Listener

--- src/Listener/ButtonViewListener.java
+++ src/Listener/ButtonViewListener.java
@@ -2,11 +2,16 @@ package Listener;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 import javax.swing.JButton;
 
 import GUI.WeekPlanVeiwer;
 import GUI.Windowframe;
+import Manager.WeekPlanManager;
 
 public class ButtonViewListener implements ActionListener {
 	Windowframe f;
@@ -17,10 +22,38 @@ public class ButtonViewListener implements ActionListener {
 
 	
 	public void actionPerformed(ActionEvent e) {
-		JButton b = (JButton)e.getSource();
-		WeekPlanVeiwer viewer = f.getWv();
-		f.setupPanel(viewer);
+		WeekPlanVeiwer wv = f.getWv();
+		WeekPlanManager weekplanmanager = getobject("weekplanmanager.ser");
+		wv.setW(weekplanmanager);
+		
+		f.getContentPane().removeAll();
+		f.getContentPane().add(wv);
+		f.revalidate();
+		f.repaint();
 		
 	}
+	public static WeekPlanManager getobject(String filename) {
+		WeekPlanManager weekplanmanager = null;
+		
+		
+		FileInputStream file;
+		try {
+			file = new FileInputStream(filename);
+			ObjectInputStream in = new ObjectInputStream(file);
+			 weekplanmanager=(WeekPlanManager)in.readObject();
+			 in.close();
+			 file.close();
+		} catch (FileNotFoundException e) {
+			return weekplanmanager;
+		}
+		 catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		return weekplanmanager;
+	}
+	
 
 }
